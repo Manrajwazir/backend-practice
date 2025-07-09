480f025250f21fc84237b21e31d4dedfa441c12e
Manrajwazir <manrajwazir@gmail.com>
2025-07-08 18:14:54 -0600

    done with server.js for now

--- simple-todo-app/src/server.js
+++ simple-todo-app/src/server.js
@@ -10,10 +10,11 @@ const __filename = fileURLToPath(import.meta.url)
 // get the directory name from the file path
 const __dirname = dirname(__filename)
 
+// middleware
+app.use(express.json())
 // serve the html file from the /public directory
-//tells express to servce all files from the public folder as static assets/ files
-//any requests for the css files will be resolved to the public directory.
-
+// tells express to servce all files from the public folder as static assets/ files
+// any requests for the css files will be resolved to the public directory.
 app.use(express.static(path.join(__dirname, '../public')))
 
 // serving up the html file from the /publiv public directory
