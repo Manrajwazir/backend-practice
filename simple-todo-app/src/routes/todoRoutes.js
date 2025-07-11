722f5e9a5752e5f660716ed560768187c3a2773b
Manrajwazir <manrajwazir@gmail.com>
2025-07-11 00:47:59 -0600

    made get users todos endpoint and added middleware for security

--- simple-todo-app/src/routes/todoRoutes.js
+++ simple-todo-app/src/routes/todoRoutes.js
@@ -4,12 +4,14 @@ import db from '../db.js'
 const router = express.Router()
 
 // Get all todos for the logged in user
-router.get('/', (req, res) => {})
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
 
 // create a new todo
-router.post('/', (req, res) => {}
-
-)
+router.post('/', (req, res) => {})
 
 // update a todo
 router.put('/:id', (req, res) => {})
